2e37ec0c404d4b346f88623bfe2c7eb51400828b
Angelica <angelicanorlander@telia.com>
2024-11-28 22:01:01 +0100

    functional addItem()  to add items to the list

--- js/scripts.js
+++ js/scripts.js
@@ -6,23 +6,31 @@
 
 //When button + is pushed - the product should be added to cart list. 
 
-function addItem(){}
-
+//This function adds new listItem
+function addItem(itemName) {
+    const items_list = document.getElementById("items_list");
+    const newListItem = document.createElement("li");
+    newListItem.textContent = itemName;
+    items_list.appendChild(newListItem);
+}
 //When button CART is pushed - You should go to checkout
 
 function goToCheckout (){}
 
 //When button TA BORT is pushed - You will remove one item
 
-function removeItem (){}
+function removeItem (){
+   
+    
+}
 
 //When button LÄGG TILL is pushed - You will add one of the same item 
 
-function addItem(){} //Could this work as the first one?
+//function addItem(){} //Could this work as the first one?
 
 
 // When button Swish is pushed - You will get something that says "You have paid" and then removes everything from cart and goes to first page. 
-
+//This function clears the list by ID: "items_list".
 function payItems(){
     alert ("Tack för att du har handlat hos oss!");
     const items_list = document.getElementById("items_list");
