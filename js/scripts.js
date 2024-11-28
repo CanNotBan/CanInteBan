81838a0f91ba3204148e8e455901ae692652da2d
Angelica <angelicanorlander@telia.com>
2024-11-28 15:08:31 +0100

    Initial pseudocode draft for button functionality

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+
+
+
+
+//ButtonLogic -
+
+//When button + is pushed - the product should be added to cart. 
+
+//When button CART is pushed - You should go to checkout
+
+//When button TA BORT is pushed - You will remove one item
+
+//When button LÄGG TILL is pushed - You will add one of the same item
+
+// When button Swish is pushed - You will get something that says "You have paid" and then removes everything from cart and goes to first page. 
+
+
+//Might need a button GÅ TILLBAKA - to go from checkout to mainpage.
