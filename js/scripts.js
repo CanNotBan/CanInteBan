a39ba0c48ee599a417a98c8622e72ef4e890c59f
Angelica <angelicanorlander@telia.com>
2024-11-28 15:19:13 +0100

    Add function placeholders for button logic

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,28 @@
 
 //ButtonLogic -
 
-//When button + is pushed - the product should be added to cart. 
+//When button + is pushed - the product should be added to cart list. 
+
+function addItem(){}
 
 //When button CART is pushed - You should go to checkout
 
+function goToCheckout (){}
+
 //When button TA BORT is pushed - You will remove one item
 
-//When button LÄGG TILL is pushed - You will add one of the same item
+function removeItem (){}
+
+//When button LÄGG TILL is pushed - You will add one of the same item 
+
+function addItem(){} //Could this work as the forst one?
+
 
 // When button Swish is pushed - You will get something that says "You have paid" and then removes everything from cart and goes to first page. 
 
+function payItems(){}
+
 
 //Might need a button GÅ TILLBAKA - to go from checkout to mainpage.
+
+function goToMainpage (){}
